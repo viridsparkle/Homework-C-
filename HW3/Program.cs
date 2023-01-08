7786d14096d17552665c11f1ac415bd4916090f3
olgachumakova <virid.sparkle@gmail.com>
2023-01-08 17:02:57 +0400

    Отредактировали условия задач

---
+++ HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = -9 b = -3 -> max = -3
+
+int a = -9;
+int b = -3;
+
+int max = a;
+int min = b;
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
