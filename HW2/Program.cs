a11f2fb1b33b3d801da4260556daa3657c3f7614
olgachumakova <virid.sparkle@gmail.com>
2023-01-08 16:42:35 +0400

    Вторая задача

---
+++ HW2/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 2;
+int b = 10;
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
+}
