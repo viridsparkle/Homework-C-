a9d171b6f5133c920407f517d3ae8780dc7bca27
olgachumakova <virid.sparkle@gmail.com>
2023-01-09 20:15:37 +0400

    Задача 2

---
+++ HW2.Ex.2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int a = 78;
+
+string str = a.ToString();
+int lenght = str.Length;
+int findIndex = 2;
+
+if (findIndex < lenght)
+{
+    Console.WriteLine(str[findIndex]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
