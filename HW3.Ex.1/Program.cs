ac1232d6e19b5a880c71f59fe03c44b263972b5e
olgachumakova <virid.sparkle@gmail.com>
2023-01-12 15:52:18 +0400

    Задача 1

---
+++ HW3.Ex.1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+double GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Convert.ToInt32(GetNumber("Пятизначное число = "));
+int ClassOfNumber = 10000;
+
+// void Answer(int number, int ClassOfNumber)
+{
+    while (number < 100000 && ClassOfNumber >= 1)
+    {
+        int lastN = number % 10;
+        int firstN = number / ClassOfNumber;
+        if (lastN == firstN)
+        {
+            number = number % ClassOfNumber / 10;
+            ClassOfNumber = ClassOfNumber / 100;
+        }
+        else
+        {
+            System.Console.WriteLine("Число не является палиндромом");
+            break;
+        }
+    }
+    if (ClassOfNumber == 0)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    
+}
+
+
+
+// System.Console.WriteLine(Answer(number, ClassOfNumber));
+
+
+
