afb07ef5efacbd31324c11e0a5f178485be70a4f
olgachumakova <virid.sparkle@gmail.com>
2023-01-20 19:54:24 +0400

    HW6

---
+++ HW6.Ex.1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// 1. Ввод M
+// 2. Ввод чисел и подсчет их количества больше 0
+// 3. Распечатать результат
+
+// 1. Ввод M
+int GetNumberByUser(string text)
+{
+    System.Console.WriteLine("Введите число : ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2. Ввод чисел и подсчет их количества больше 0
+int GetCountOfNumbersAbove0(int n)
+{
+    int number = 0;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+        // System.Console.Write(PrintNumbers(number));
+        if (number > 0) count++;
+    }
+    return count;
+}
+
+// 3. Распечатать результат
+string PrintResult(int result)
+{
+    return $"{("Количество чисел больше 0 = ")} {result}";
+}
+
+int m = GetNumberByUser("");
+int count = GetCountOfNumbersAbove0(m);
+System.Console.WriteLine(PrintResult(count));
---
+++ HW6.Ex.2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1x + b1 = k2x + b2
+// k1 * x - k2 * x = b2 - b1
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+// int[] GetData(int size);
+// {
+//     System.Console.WriteLine("b1 = ");
+//     int b1 = Convert.ToInt32(Console.ReadLine());
+//     System.Console.WriteLine("k1 = ");
+//     int k1 = Convert.ToInt32(Console.ReadLine());
+//     System.Console.WriteLine("b2 = ");
+//     int b2 = Convert.ToInt32(Console.ReadLine());
+//     System.Console.WriteLine("k2 = ");
+//     int k2 = Convert.ToInt32(Console.ReadLine());
+//     return new int[size] {b1, k1, b2, k2};
+// }
+
+// int[] data = GetData();
+
+// 1. Ввод N
+int GetNumberByUser(string text)
+{
+    System.Console.WriteLine("Введите число : ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2. Создание массива
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+// 3. Заполнение массива (ввод данных с консоли)
+void GetArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        // if (i = 0) System.Console.WriteLine($"{("b1 = ")}");
+        // if (i = 1) System.Console.WriteLine("k1 = ");
+        // if (i = 2) System.Console.WriteLine("b2 = ");
+        // if (i = 3) System.Console.WriteLine("k2 = ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
