8595a0e2c50cc7b368b75af892a8af1ee8c23b84
olgachumakova <virid.sparkle@gmail.com>
2023-01-20 21:00:03 +0400

    HW6

--- HW6.Ex.2/Program.cs
+++ HW6.Ex.2/Program.cs
@@ -24,30 +24,44 @@
 
 // int[] data = GetData();
 
-// 1. Ввод N
-int GetNumberByUser(string text)
+
+// 1. Создание массива
+double[] CreateArray(int size)
 {
-    System.Console.WriteLine("Введите число : ");
-    return Convert.ToInt32(Console.ReadLine());
+    return new double[size];
 }
 
-// 2. Создание массива
-int[] CreateArray(int size)
-{
-    return new int[size];
-}
-
-// 3. Заполнение массива (ввод данных с консоли)
-void GetArray(int[] numbers)
+// 2. Заполнение массива (ввод данных с консоли)
+void GetArray(double[] numbers)
 {
     for (int i = 0; i < numbers.Length; i++)
     {
-        // if (i = 0) System.Console.WriteLine($"{("b1 = ")}");
+        // if (i = 0) 
+        // {
+        //     System.Console.WriteLine($"b1 = ");
+        // }
         // if (i = 1) System.Console.WriteLine("k1 = ");
         // if (i = 2) System.Console.WriteLine("b2 = ");
         // if (i = 3) System.Console.WriteLine("k2 = ");
-        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        numbers[i] = Convert.ToDouble(Console.ReadLine());
     }
 }
 
+// 3. Метод получения координат точки пересечения прямых
+(double x, double y) GetXY(double[] array)
+{
+    double b1 = array[0];
+    double k1 = array[1];
+    double b2 = array[2];
+    double k2 = array[3];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+double[] array = CreateArray(4);
+GetArray(array);
+
+(double x, double y) point = GetXY(array);
+System.Console.WriteLine($"({point.x}; {point.y})");
 
